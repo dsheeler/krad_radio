14aaa5c9d62b701097131fe574d01bac27e3426d
Daniel Sheeler <dsheeler@pobox.com>
2012-12-04 09:30:16 -0500

    adding a field for uptime

--- lib/krad_radio/krad_radio_common.h
+++ lib/krad_radio/krad_radio_common.h
@@ -4,11 +4,14 @@
 typedef struct krad_radio_rep_St krad_radio_rep_t;
 
 struct krad_radio_rep_St {
-	
-	uint32_t remote_port;
-	uint32_t web_port;
-	uint32_t osc_port;
-	
+  
+  uint32_t remote_port;
+  uint32_t web_port;
+  uint32_t osc_port;
+  uint64_t start_time;
+  
+  char logname[256];
+  
 };
 
 #endif // KRAD_RADIO_COMMON_H
