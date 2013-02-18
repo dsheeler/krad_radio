d7d8d50bb4195e00867a704843b155d0a53b2f0a
David Richards <drichards@mozilla.com>
2013-02-18 11:26:34 -0800

    wayrad crate counter edition

--- clients/wayrad/wayrad.c
+++ clients/wayrad/wayrad.c
@@ -1,5 +1,7 @@
 #include "wayrad.h"
 
+static void render_cratezone (wayrad_t *wayrad, uint32_t time, void *buffer);
+
 int wayrad_frame (void *pointer, uint32_t time) {
 
 	int updated;
@@ -8,17 +10,111 @@ int wayrad_frame (void *pointer, uint32_t time) {
 	
 	wayrad_t *wayrad = (wayrad_t *)pointer;
 	
-	hexagon (wayrad->width, wayrad->height,
-	         wayrad->krad_wayland->display->pointer_x,
-	         wayrad->krad_wayland->display->pointer_y,
-	         time, wayrad->buffer);
+	//hexagon (wayrad->width, wayrad->height,
+	//         wayrad->krad_wayland->display->pointer_x,
+	//         wayrad->krad_wayland->display->pointer_y,
+	//         time, wayrad->buffer);
 
-  updated = 1;
 
-	printf ("frame callback time is %u\r", time);
-	fflush (stdout);
+  if (wayrad->cratezone.crates != wayrad->cratezone.crates_last) {
+    render_cratezone (wayrad, time, wayrad->buffer);
+    wayrad->cratezone.crates_last = wayrad->cratezone.crates;
+    updated = 1;
+  }
+
+	//printf ("frame callback time is %u\r", time);
+	//fflush (stdout);
 
 	return updated;
+}
+
+static void render_cratezone (wayrad_t *wayrad, uint32_t time, void *buffer) {
+
+	cairo_surface_t *cst;
+	cairo_t *cr;
+	char text[256];
+
+	cst = cairo_image_surface_create_for_data ((unsigned char *)buffer,
+												 CAIRO_FORMAT_ARGB32,
+												 wayrad->width,
+												 wayrad->height,
+												 wayrad->width * 4);
+	
+	cr = cairo_create (cst);
+
+  cairo_save (cr);
+	cairo_set_source_rgba (cr, BGCOLOR_CLR);
+	cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);	
+	cairo_paint (cr);
+	cairo_restore (cr);
+
+
+  sprintf (text, "Crates: %"PRIu64"", wayrad->cratezone.crates);
+
+  cairo_select_font_face (cr, "Helvetica", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+  cairo_set_font_size (cr, 100);
+  cairo_set_source_rgba (cr, GREY2, 0.8);
+  
+  cairo_move_to (cr, 128, 320);
+  cairo_show_text (cr, text);
+  cairo_stroke (cr);
+
+	cairo_surface_flush (cst);
+	cairo_destroy (cr);
+	cairo_surface_destroy (cst);
+}
+
+void get_delivery (wayrad_t *wayrad) {
+
+  kr_crate_t *crate;
+  crate = NULL;
+
+  kr_delivery_get (wayrad->client, &crate);
+
+  if (crate != NULL) {
+    //if (kr_crate_loaded (crate)) {
+      //if ((crate->addr->path.unit == KR_MIXER) && 
+      //    (crate->addr->path.subunit.mixer_subunit == KR_PORTGROUP)) {
+        wayrad->cratezone.crates++;
+      //}
+    //}
+    kr_crate_recycle (&crate);
+  }
+}
+
+void *deliveries_thread (void *arg) {
+
+  int b;
+  int ret;
+  uint64_t max;
+  unsigned int timeout_ms;
+  wayrad_t *wayrad;
+  
+  wayrad = (wayrad_t *)arg;
+    
+  ret = 0;
+  b = 0;
+  max = 1000;
+  timeout_ms = 3000;
+  
+
+  kr_subscribe_all (wayrad->client);
+  
+  printf ("Waiting for up to %"PRIu64" deliveries for up to %ums each\n",
+          max, timeout_ms);
+  
+  for (b = 0; b < max; b++) {
+    ret = kr_wait (wayrad->client, timeout_ms);
+    if (ret > 0) {
+      get_delivery (wayrad);
+    } else {
+      printf (".");
+      fflush (stdout);
+    }
+  }
+  
+  wayrad->done = 1;
+  return NULL;  
 }
 
 void wayrad_run (wayrad_t *wayrad) {
@@ -29,7 +125,15 @@ void wayrad_run (wayrad_t *wayrad) {
   
   count = 0;
 
-  while (count < 5000) {
+
+  pthread_create (&wayrad->deliveries_thread,
+                  NULL,
+                  deliveries_thread,
+                  (void *)wayrad);
+
+  pthread_detach (wayrad->deliveries_thread);
+
+  while (!wayrad->done) {
     krad_wayland_iterate (wayrad->krad_wayland);
     
     if ((wayrad->krad_wayland->mousein) && (wayrad->krad_wayland->click)) {
--- clients/wayrad/wayrad.h
+++ clients/wayrad/wayrad.h
@@ -4,6 +4,14 @@
 #include "hex.h"
 
 typedef struct wayrad_St wayrad_t;
+typedef struct cratezone_St cratezone_t;
+
+struct cratezone_St {
+
+  uint64_t crates;
+  uint64_t crates_last;
+
+};
 
 struct wayrad_St {
 
@@ -17,6 +25,9 @@ struct wayrad_St {
   char sysname[96];
   kr_client_t *client;
 
+  cratezone_t cratezone;
+  pthread_t deliveries_thread;
+  int done;
 };
 
 void wayrad_run (wayrad_t *wayrad);
