dfcddfb0e40aa37338b6748f1d9a7ea35b299f67
David Richards <drichards@mozilla.com>
2013-03-03 13:33:50 -0800

    add choas monkey

---
+++ clients/khaos_monkey.c
@@ -0,0 +1,268 @@
+#include "kr_client.h"
+
+#define MAX_MONKEYS 4
+
+typedef struct khaos_monkey_St khaos_monkey_t;
+
+struct khaos_monkey_St {
+  kr_client_t *client;
+  int lifetime;
+	pthread_t on_the_lamb_thread;
+};
+
+void my_remote_print (kr_remote_t *remote) {
+  printf ("oh its a remote! %d on interface %s\n",
+          remote->port,
+          remote->interface);
+}
+
+void my_tag_print (kr_tag_t *tag) {
+  printf ("The tag I wanted: %s - %s\n",
+          tag->name,
+          tag->value);
+}
+
+void my_portgroup_print (kr_portgroup_t *portgroup) {
+  printf ("oh its a portgroup called %s and the volume is %0.2f%%\n",
+           portgroup->sysname,
+           portgroup->volume[0]);
+}
+
+void my_compositor_print (kr_compositor_t *compositor) {
+
+  printf ("Compositor Resolution: %d x %d Frame Rate: %d / %d - %f\n",
+					 compositor->width, compositor->height,
+					 compositor->fps_numerator, compositor->fps_denominator,
+					 ((float)compositor->fps_numerator / (float)compositor->fps_denominator));
+}
+
+void my_mixer_print (kr_mixer_t *mixer) {
+
+  printf ("Mixer Sample Rate: %d\n",
+					 mixer->sample_rate);
+}
+
+void my_print (kr_crate_t *crate) {
+
+  if ((crate->addr->path.unit == KR_MIXER) && (crate->addr->path.subunit.zero == KR_UNIT)) {
+    my_mixer_print (crate->inside.mixer);
+  }
+  if ((crate->addr->path.unit == KR_COMPOSITOR) && (crate->addr->path.subunit.zero == KR_UNIT)) {
+    my_compositor_print (crate->inside.compositor);
+  }
+  if ((crate->addr->path.unit == KR_MIXER) && (crate->addr->path.subunit.mixer_subunit == KR_PORTGROUP)) {
+    my_portgroup_print (crate->inside.portgroup);
+  }
+}
+
+void get_delivery (kr_client_t *client) {
+
+  kr_crate_t *crate;
+  char *string;
+  int integer;
+  float real;
+
+  integer = 0;
+  real = 0.0f;
+  string = NULL;
+  crate = NULL;
+  //printf ("*** Delivery Start: \n");
+
+  kr_delivery_get (client, &crate);
+
+  if (crate != NULL) {
+
+    //kr_address_debug_print (crate->addr); 
+
+    /* Crate sometimes can be converted
+       to a integer, float or string */
+    
+    if (kr_uncrate_string (crate, &string)) {
+      //printf ("String: \n%s\n", string);
+      kr_string_recycle (&string);
+    }
+    
+    if (kr_uncrate_int (crate, &integer)) {
+      //printf ("Int: %d\n", integer);
+      /* or but check first always! */
+      // if (kr_crate_has_int (crate)) {
+      //   crate->integer;
+      // }
+    }
+    
+    if (kr_uncrate_float (crate, &real)) {
+      //printf ("Float: %f\n", real);
+      /* or but check first always! */
+      // if (kr_crate_has_float (crate)) {
+      //   crate->real;
+      // }
+    }
+    
+    //crate->notice  << a type/reason/event    
+    
+    /* Crate has a rep struct */
+    
+    if (kr_crate_loaded (crate)) {
+      //my_print (crate);
+    }
+
+    kr_crate_recycle (&crate);
+    //printf ("*** Delivery End\n\n\n");
+  }
+}
+
+void accept_some_deliveries (kr_client_t *client) {
+
+  int wait_ms;
+  
+  wait_ms = 250;
+
+  while (kr_delivery_wait_until_final (client, wait_ms)) {
+    get_delivery (client);
+  }
+}
+
+void rage (kr_client_t *client) {
+
+  //kr_tags (client, NULL);    
+  //kr_delivery_accept_and_report (client);
+
+  kr_system_info (client);
+  accept_some_deliveries (client);
+
+  kr_remote_list (client);
+  accept_some_deliveries (client);
+
+  kr_compositor_info (client);
+  accept_some_deliveries (client);
+  
+  kr_mixer_info (client);
+  accept_some_deliveries (client);
+
+  kr_mixer_portgroups (client);
+  accept_some_deliveries (client);
+}
+
+void *monkey_around (void *arg) {
+
+  khaos_monkey_t *monkey = (khaos_monkey_t *)arg;
+
+  int b;
+  int ret;
+  uint64_t max;
+  unsigned int timeout_ms;
+  
+  ret = 0;
+  b = 0;
+  max = rand() % 800;
+  timeout_ms = 3000;
+  
+  kr_subscribe_all (monkey->client);
+
+  //printf ("Running the one shot demo\n");
+  rage (monkey->client);
+
+
+  kr_subscribe_all (monkey->client);
+  
+  //printf ("Waiting for up to %"PRIu64" deliveries for up to %ums each\n",
+  //        max, timeout_ms);
+  
+  for (b = 0; b < max; b++) {
+    ret = kr_wait (monkey->client, timeout_ms);
+    if (ret > 0) {
+      get_delivery (monkey->client);
+    } else {
+      //printf (".");
+      //fflush (stdout);
+    }
+    
+    if ((b % 1423) > 1222) {
+      rage (monkey->client);
+    }
+  }
+  
+  kr_disconnect (monkey->client);
+  kr_client_destroy (&monkey->client);
+
+  return NULL;
+}
+
+khaos_monkey_t *monkey_on_the_lamb (char *sysname) {
+
+  khaos_monkey_t *monkey;
+  
+  monkey = calloc (1, sizeof(khaos_monkey_t));
+
+  monkey->client = kr_client_create ("khoas kokonut");
+
+  if (monkey->client == NULL) {
+    fprintf (stderr, "Could create client\n");
+    exit (1);
+  }
+
+  if (!kr_connect (monkey->client, sysname)) {
+    fprintf (stderr, "Could not connect to %s krad radio daemon\n", sysname);
+    kr_client_destroy (&monkey->client);
+    exit (1);
+  }
+
+  printf ("Connected to %s!\n", sysname);
+
+  printf ("Now getting into the monkey business\n");
+
+	pthread_create (&monkey->on_the_lamb_thread, NULL, monkey_around, (void *)monkey);
+	
+	return monkey;
+}
+
+void catch_monkey (khaos_monkey_t *monkey) {
+  pthread_join (monkey->on_the_lamb_thread, NULL);
+  free (monkey);   
+}
+
+void uncage_monkeys (char *sysname) {
+
+  int m;
+  int lamb;
+  khaos_monkey_t *monkeys[MAX_MONKEYS];
+  
+  lamb = rand() % MAX_MONKEYS;
+    
+  for (m = 0; m < lamb; m++) {
+    monkeys[m] = monkey_on_the_lamb (sysname);
+  }
+  
+  for (m = 0; m < lamb; m++) {
+    catch_monkey (monkeys[m]);
+  }
+}
+
+int main (int argc, char *argv[]) {
+
+  char *sysname;
+
+  sysname = NULL;
+
+  if (argc < 2) {
+    fprintf (stderr, "Specify a station sysname!\n");
+    return 1;
+  }
+
+  if (krad_valid_host_and_port (argv[1])) {
+    sysname = argv[1];
+  } else {
+    if (!krad_valid_sysname(argv[1])) {
+      fprintf (stderr, "Invalid station sysname!\n");
+      return 1;
+    } else {
+      sysname = argv[1];
+    }
+  }
+
+  printf ("Warning! This is a Chaos Monkey!\n");
+
+  uncage_monkeys (sysname);
+  
+  return 0;
+}
