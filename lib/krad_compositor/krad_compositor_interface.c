7f831deac361ed87b01ad1843b585b1626468177
David Richards <drichards@mozilla.com>
2013-05-03 05:03:59 -0700

    some whitespace

--- lib/krad_compositor/krad_compositor_interface.c
+++ lib/krad_compositor/krad_compositor_interface.c
@@ -195,7 +195,7 @@ int krad_compositor_command ( kr_io2_t *in, kr_io2_t *out, krad_radio_client_t *
 
   printk ("comp command");
 
-	switch ( command ) {
+  switch ( command ) {
     case EBML_ID_KRAD_COMPOSITOR_CMD_SET_SUBUNIT:
       unit_control.address.path.unit = KR_COMPOSITOR;
       kr_ebml2_unpack_element_uint32 (&ebml_in, &element, &numbers[0]);
