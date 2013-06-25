498f07b97b64b8f1655f4d00203da8d57ae7124f
David Richards <drichards@mozilla.com>
2013-06-25 11:22:52 -0700

    misc

--- lib/krad_web/stream.c
+++ lib/krad_web/stream.c
@@ -1,8 +1,21 @@
 int32_t interweb_client_get_stream(kr_iws_client_t *client) {
 
-  //client->type = KR_IWS_STREAM_OUT;
+  if (strncmp(client->get, "/fakestream.bs", 14)) {
+    client->type = KR_IWS_STREAM_OUT;
+    return 1;
+  } else {
+    printk("fake looking for stream %s and not finding it", client->get);
+  }
+  return 0;
+}
 
-  printk("fake looking for stream %s and not finding it", client->get);
+int32_t krad_interweb_stream_client_handle(kr_iws_client_t *client) {
+
+  size_t fakebytes;
+
+  fakebytes = rand() % 87;
+
+  printk("fake streaming %zu bytes", fakebytes);
 
   return 0;
 }
