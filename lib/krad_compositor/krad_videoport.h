c6787cb7d16e0b3e3f3f4fdc669e1a20a5bbaafa
Daniel Sheeler <six600110@pobox.com>
2013-06-26 01:38:11 -0400

    add missing function prototypes for perspective correction funcs

--- lib/krad_compositor/krad_videoport.h
+++ lib/krad_compositor/krad_videoport.h
@@ -56,4 +56,22 @@ struct krad_compositor_port_St {
   int socketpair[2];
 };
 
+void krad_compositor_videoport_set_view_top_left_x (krad_compositor_port_t 
+ *videoport, int view_top_left_x); 
+void krad_compositor_videoport_set_view_top_left_y (krad_compositor_port_t 
+ *videoport, int view_top_left_y); 
+void krad_compositor_videoport_set_view_top_right_x (krad_compositor_port_t 
+ *videoport, int view_top_right_x); 
+void krad_compositor_videoport_set_view_top_right_y (krad_compositor_port_t 
+ *videoport, int view_top_right_y); 
+void krad_compositor_videoport_set_view_bottom_left_x (krad_compositor_port_t 
+ *videoport, int view_bottom_left_x); 
+void krad_compositor_videoport_set_view_bottom_left_y (krad_compositor_port_t 
+ *videoport, int view_bottom_left_y); 
+void krad_compositor_videoport_set_view_bottom_right_x (krad_compositor_port_t 
+ *videoport, int view_bottom_right_x); 
+void krad_compositor_videoport_set_view_bottom_right_y (krad_compositor_port_t
+ *videoport, int view_bottom_right_y); 
+
+
 #endif // KRAD_COMPOSITOR_PORT_H
