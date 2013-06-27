85a624d6c2108cff3048ccaed5ba2ba800d0f1f4
David Richards <drichards@mozilla.com>
2013-06-27 02:26:59 -0700

    add autodel script

---
+++ autodel.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -f -rf autom4te.cache
+rm -f config.log
+rm -f missing
+rm -f INSTALL
+rm -f Makefile.in
+rm -f NEWS
+rm -f aclocal.m4
+rm -f autom4te.cache/
+rm -f compile
+rm -f config.h.in
+rm -f config.log
+rm -f configure
+rm -f install-sh
+rm -f missing
