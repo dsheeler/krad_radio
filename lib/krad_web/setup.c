bec5c33ce446691b2d0ae11cd824b45e1a4967bb
David Richards <drichards@mozilla.com>
2013-07-05 00:47:45 -0700

    whitespace / style fixes

--- lib/krad_web/setup.c
+++ lib/krad_web/setup.c
@@ -6,7 +6,7 @@ char *kr_interweb_server_load_file_or_string(char *input) {
   struct stat file_stat;
   int bytes_read;
   int ret;
-  
+
   fd = 0;
   bytes_read = 0;
   string = NULL;
@@ -19,7 +19,7 @@ char *kr_interweb_server_load_file_or_string(char *input) {
 
     fd = open (input, O_RDONLY);
     if (fd < 1) {
-      printke("could not open");    
+      printke("could not open");
       return NULL;
     }
     fstat (fd, &file_stat);
@@ -30,10 +30,10 @@ char *kr_interweb_server_load_file_or_string(char *input) {
       return NULL;
     }
 
-    string = calloc (1, length);        
+    string = calloc (1, length);
 
     while (bytes_read < length) {
-    
+
       ret = read (fd, string + bytes_read, length - bytes_read);
 
       if (ret < 0) {
@@ -60,12 +60,12 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
   int len;
   int pos;
   int template_pos;
-  
+
   template_pos = 0;
   pos = 0;
   len = 0;
   total_len = 0;
-  
+
   server->api_js = (char *)lib_krad_web_res_kr_api_js;
   server->api_js_len = lib_krad_web_res_kr_api_js_len;
   server->iface_js  = (char *)lib_krad_web_res_kr_interface_js;
@@ -76,7 +76,7 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
   memset (string, 0, sizeof(string));
   snprintf (string, 7, "%d", server->uberport);
   total_len += strlen(string);
-  
+
   html_template = (char *)lib_krad_web_res_krad_radio_html;
   html_template_len = lib_krad_web_res_krad_radio_html_len - 1;
   total_len += html_template_len - 4;
@@ -87,26 +87,26 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
    kr_interweb_server_load_file_or_string(server->htmlheader_source);
   server->htmlfooter =
    kr_interweb_server_load_file_or_string(server->htmlfooter_source);
-  
+
   if (server->headcode != NULL) {
     total_len += strlen(server->headcode);
   }
   if (server->htmlheader != NULL) {
-    total_len += strlen(server->htmlheader);    
+    total_len += strlen(server->htmlheader);
   }
   if (server->htmlfooter != NULL) {
-    total_len += strlen(server->htmlfooter);    
+    total_len += strlen(server->htmlfooter);
   }
 
   server->html_len = total_len;
-  server->html = calloc (1, server->html_len + 1);
-  
+  server->html = malloc(server->html_len + 1);
+
   len = strcspn (html_template, "~");
   strncpy (server->html, html_template, len);
   strcpy (server->html + len, string);
   pos = len + strlen(string);
   template_pos = len + 1;
-  
+
   len = strcspn (html_template + template_pos, "~");
   strncpy (server->html + pos, html_template + template_pos, len);
   template_pos += len + 1;
@@ -116,7 +116,7 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
     strncpy (server->html + pos, server->headcode, len);
     pos += len;
   }
-  
+
   len = strcspn (html_template + template_pos, "~");
   strncpy (server->html + pos, html_template + template_pos, len);
   template_pos += len + 1;
@@ -126,7 +126,7 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
     strncpy (server->html + pos, server->htmlheader, len);
     pos += len;
   }
-  
+
   len = strcspn (html_template + template_pos, "~");
   strncpy (server->html + pos, html_template + template_pos, len);
   template_pos += len + 1;
@@ -136,12 +136,12 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
     strncpy (server->html + pos, server->htmlfooter, len);
     pos += len;
   }
-  
+
   len = html_template_len - template_pos;
   strncpy (server->html + pos, html_template + template_pos, len);
   template_pos += len;
-  pos += len;  
-  
+  pos += len;
+
   if (template_pos != html_template_len) {
     failfast("html template miscalculation: %d %d", template_pos, html_template_len);
   }
@@ -154,14 +154,14 @@ void kr_interweb_server_setup_html (kr_interweb_t *server) {
 
   if (server->headcode != NULL) {
     free (server->headcode);
-    server->headcode = NULL;    
+    server->headcode = NULL;
   }
   if (server->htmlheader != NULL) {
     free (server->htmlheader);
-    server->htmlheader = NULL;    
+    server->htmlheader = NULL;
   }
   if (server->htmlfooter != NULL) {
     free (server->htmlfooter);
-    server->htmlfooter = NULL;    
-  }    
+    server->htmlfooter = NULL;
+  }
 }
