9ed656e736db6adcc34e87401ffed9fb889343de
David Richards <drichards@mozilla.com>
2013-08-25 15:14:51 -0700

    update autotools and version number

--- lib/krad_radio/krad_radio_version.h
+++ lib/krad_radio/krad_radio_version.h
@@ -1,4 +1,4 @@
-#define VERSION_NUMBER 22
+#define VERSION_NUMBER 23
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
---
+++ scripts/krad_ver.sh
@@ -0,0 +1,1 @@
+tr -d [:alpha:][:punct:][:space:] < lib/krad_radio/krad_radio_version.h
