0b37630e09e1aa6d07a5bf15d292d6e19e2e1b93
David Richards <drichards@mozilla.com>
2013-08-25 05:17:55 -0700

    ticking clock

--- lib/krad_transponder/krad_transponder_processor.c
+++ lib/krad_transponder/krad_transponder_processor.c
@@ -12,5 +12,12 @@ void xpdr_adapter_path_av_cb(kr_adapter_path_av_cb_arg *arg) {
 
 void xpdr_adapter_av_cb(kr_adapter_av_cb_arg *arg) {
 
-  printk("yay adapter av cb!");
+  kr_xpdr *xpdr;
+  uint32_t ret;
+
+  xpdr = (kr_xpdr *)arg->user;
+  if (1) { /* if we are the clock .. */
+    ret = kr_mixer_process(xpdr->mixer);
+    /* printk("mixed %u frames", ret); */
+  }
 }
