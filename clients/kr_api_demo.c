93d80a79abba2385b6d0c880d3fa4ea376efc77f
David Richards <drichards@mozilla.com>
2013-08-25 18:35:02 -0700

    printf annoyance fix

--- clients/kr_api_demo.c
+++ clients/kr_api_demo.c
@@ -148,7 +148,7 @@ void get_delivery(kr_client_t *client) {
 
   while((kr_delivery_get(client, &crate) > 0) &&
         (crate != NULL)) {
-    printf("Time is: %zu\n", time(NULL));
+    printf("Time is: %"PRIu64"\n", krad_unixtime(NULL));
     handle_crate(crate);
     kr_crate_recycle(&crate);
   }
