bfdfbc1743f0167f039b05fd2d7d3610f83c961a
David Richards <drichards@mozilla.com>
2013-10-16 10:14:16 -0700

    update embeder script again

--- scripts/update_embeded.sh
+++ scripts/update_embeded.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ ! -f "tools/hextool/hextool" ]; then
+#if [ ! -f "tools/hextool/hextool" ]; then
   cd tools/hextool
   gcc hexutils.c hextool.c -g -o hextool
   cd ../../
-fi
+#fi
 
 rm -f lib/krad_web/embed.h
 cd lib/krad_web/core
+rm -f interface.js
+rm -f *.h
 ../../../tools/hextool/hextool api.js
 ../../../tools/hextool/hextool index.html
 ../../../tools/hextool/hextool dev_interface.js
@@ -15,8 +17,5 @@ cat ../rack/*.js > interface.js
 ../../../tools/hextool/hextool interface.js
 cat *.h > ../embed.h
 rm -f interface.js
-rm -f kr_interface_js.h
-rm -f kr_dev_interface_js.h
-rm -f kr_index_html.h
-rm -f kr_api_js.h
+rm -f *.h
 cd ../../../
