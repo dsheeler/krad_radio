827b7b27a223dc2eaa5277042c5963e633f8aa6a
David Richards <drichards@mozilla.com>
2013-12-15 21:00:25 -0800

    clean m4 dir as well

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@ if [ "$1" = "--clean" ]; then
   if [ -e "Makefile" ]; then
     make clean
   fi
-  rm -f -rf autom4te.cache
+  rm -rf autom4te.cache m4
   rm -f depcomp config.guess config.sub libtool ltmain.sh\
    config.status Makefile stamp-h1 config.h.in~ config.h\
    config.log missing INSTALL Makefile.in NEWS aclocal.m4\
