229adfd5c5ca6addb6b83a280b0f7b66f8d4e701
David Richards <drichards@mozilla.com>
2013-12-19 14:46:46 -0800

    make finding work easier

--- scripts/kr_work.sh
+++ scripts/kr_work.sh
@@ -1,1 +1,9 @@
-grep -irHn 'TODO\|FIXME' clients/ lib/ --include=*.h --include=*.c
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  paths="clients/ lib/"
+else
+  paths="$*"
+fi
+
+grep -irHn 'TODO\|FIXME' --include=*.h --include=*.c $paths
