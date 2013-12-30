0727626a942d3d411c0e8af48513a1a0454c52ad
David Richards <drichards@mozilla.com>
2013-12-30 08:24:42 -0800

    quick dirty hack to fix mixer volume and processing

--- lib/krad_transponder/krad_transponder.c
+++ lib/krad_transponder/krad_transponder.c
@@ -49,12 +49,19 @@ static void xpdr_adapter_path_event_cb(kr_adapter_path_event_cb_arg *arg) {
 
 static void xpdr_adapter_event_cb(kr_adapter_event_cb_arg *arg) {
   kr_xpdr *xpdr;
+  kr_adapter_info info;
   uint32_t ret;
   xpdr = (kr_xpdr *)arg->user;
   /* if we are the audio or video clock do a process .. */
   if (kr_adapter_prepare(arg->adapter)) {
     /* FIXME FIXME FIXME need to reconcile period size */
-//    ret = kr_mixer_process(xpdr->mixer);
+    if (kr_adapter_get_info(arg->adapter, &info)) {
+      printke("We failed to get adapter info..");
+      return;
+    }
+    if (info.api == KR_ADP_JACK) {
+      ret = kr_mixer_process(xpdr->mixer);
+    }
     if (ret > 0) {
       /* printk("mixed %u frames", ret); */
       /* number of frames mixed, so time can be dealt with */
