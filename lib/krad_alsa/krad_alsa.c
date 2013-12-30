87de0aa37e3c5f0270391e5246f07bf06a535363
David Richards <drichards@mozilla.com>
2013-12-30 02:06:11 -0800

    alsa test upd

--- lib/krad_alsa/krad_alsa.c
+++ lib/krad_alsa/krad_alsa.c
@@ -2,10 +2,15 @@
 
 struct kr_alsa {
   kr_alsa_info info;
+  snd_ctl_t *ctl;
 };
 
 int kr_alsa_destroy(kr_alsa *alsa) {
   if (!alsa) return -1;
+  if (alsa->ctl) {
+    snd_ctl_close(alsa->ctl);
+    alsa->ctl = NULL;
+  }
   printk("Krad ALSA: Destroyed card %d: %s", alsa->info.card,
    alsa->info.name);
   free(alsa);
@@ -16,7 +21,10 @@ kr_alsa *kr_alsa_create(int card) {
   kr_alsa *alsa;
   int ret;
   char *name;
+  char dev_name[8];
+  int sub_device;
   kr_alsa_info *info;
+  sub_device = 0;
   alsa = calloc(1, sizeof(kr_alsa));
   if (!alsa) return NULL;
   info = &alsa->info;
@@ -34,29 +42,13 @@ kr_alsa *kr_alsa_create(int card) {
   printk("Krad ALSA: Created card %d: %s -- %s\n", info->card, info->name,
    info->longname);
 
-  char **hints;
-  ret = snd_device_name_hint(info->card, "pcm", (void***)&hints);
+  sprintf(dev_name, "hw:%d", info->card);
+  ret = snd_ctl_open(&alsa->ctl, dev_name, sub_device);
   if (ret == 0) {
-    char** n = hints;
-    while (*n != NULL) {
-      name = snd_device_name_get_hint(*n, "NAME");
-      if (name != NULL && 0 != strcmp("null", name)) {
-        printk("pcm name: %s\n", name);
-        free(name);
-      }
-      name = snd_device_name_get_hint(*n, "DESC");
-      if (name != NULL && 0 != strcmp("null", name)) {
-        printk("pcm desc: %s\n", name);
-        free(name);
-      }
-      name = snd_device_name_get_hint(*n, "IOID");
-      if (name != NULL && 0 != strcmp("null", name)) {
-        printk("pcm ioid: %s\n", name);
-        free(name);
-      }
-      n++;
+    ret = snd_ctl_pcm_next_device(alsa->ctl, &sub_device);
+    if (sub_device >= 0) {
+      printf("got subdevice! %d\n", sub_device);
     }
-    snd_device_name_free_hint((void**)hints);
   }
   return alsa;
 }
